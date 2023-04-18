e7d393dfb5da702d99fac114821a67c1ffedb156
Gabriel <21043296+gabrielzerio@users.noreply.github.com>
2023-04-18 17:02:52 -0300

    logica pronta, falta corrigir aleatoriedade e adicionar input pro usuário!

--- back.js
+++ back.js
@@ -1,9 +1,54 @@
 let log = console.log;
 
+
 function getComputerChoice(){
-
+   const aleatorio = Math.floor(Math.random() * 9);
+   if(aleatorio>=0 && aleatorio <=3){
+    return 'scissor';
+   }
+   else if(aleatorio>3 && aleatorio <=6){
+    return 'rock';
+   }
+   else if(aleatorio>6 && aleatorio <=9){
+    return 'paper';
+   }
 }
+const computerSelection = getComputerChoice();
 
-function getPlayerChoice(){
-    
+//let sup =  [['rock'>'scissor'],
+            //['paper'>'rock'],            
+            //['scissor'>'paper']]; 
+
+function playRound(playerSelection, computerSelection){
+    const p = playerSelection;
+    const c = computerSelection;
+    const rock = 'rock'; const paper = 'paper'; const scissor = 'scissor';
+    if(playerSelection===computerSelection){
+        return 'Empate!';
+    }else{
+        if(p===rock){
+            if(c===scissor){
+                return 'you won! rock beats scissor!';
+            }
+            if(c===paper){
+                return 'you lose! paper beats rock!';
+            }
+        }
+        if(p===paper){
+            if(c===rock){
+                return 'You won! paper beats rock';
+            }
+            if(c===scissor){
+                return 'You lose! scissor beats paper';
+            }
+        }
+        if(p===scissor){
+            if(c===paper){
+                return 'You won! scissor beats paper!';
+            }
+            if(c===rock){
+                return 'You lose! rock beats scissor!';
+            }
+        }
+    }
 }
